af65496c068466d3a395f8c62a856f02583182bf
jrcoleman <jonny.coleman@nih.gov>
2023-01-31 09:49:53 -0500

    Add name to storage module output

--- src/terraform/modules/secure-storage-account/outputs.tf
+++ src/terraform/modules/secure-storage-account/outputs.tf
@@ -2,3 +2,8 @@ output "storage_id" {
   value     = azurerm_storage_account.secure.id
   sensitive = true
 }
+
+output "name" {
+  value     = azurerm_storage_account.secure.name
+  sensitive = true
+}
